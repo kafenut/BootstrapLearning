cddecb8f722dfabc6ffebe11ccb0fb6a7e08287e
kafenut <45032762+kafenut@users.noreply.github.com>
2019-05-07 16:56:39 +0800

    Add files via upload
    initialization

---
+++ bootstraplearning/bootstraplearning/app/views.py
@@ -0,0 +1,30 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def typography():
+    return render_template('typography.html')
+
+@app.route('/t')
+def table():
+    return render_template('table.html')
+
+@app.route('/i')
+def image():
+    return render_template('image.html')
+
+@app.route('/c')
+@app.route('/c1')
+def component1():
+    return render_template('component1.html')
+
+@app.route('/c2')
+def component2():
+    return render_template('component2.html')
+
+@app.route('/c3')
+def component3():
+    return render_template('component3.html')
+
+
+
